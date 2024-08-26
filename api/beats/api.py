d9204c4de683f1720ce2f26cd47d1e89ec5c9ddf
Daniel Villery <daniel.villery@i-qlair.com>
2024-08-26 13:44:51 -0400

    added db session to api. fleshing out post method for beats

--- api/beats/api.py
+++ api/beats/api.py
@@ -1,9 +1,22 @@
 from fastapi import APIRouter, HTTPException, status
 from pydantic import BaseModel
-from libraries.beats import (Beat,)
+from libraries.beats import (Beat, BeatBase)
+from database import engine, SessionLocal
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 app = APIRouter(
     prefix="/beats",
     tags=["beats"],
 )
+
+
+@app.post('/beats/')
+async def create_beat(Beat: BeatBase):
